be69b9cf8374f409d6b71e91849daa6210dc5df0
AlienHeadWars <bantam2781@gmail.com>
2015-02-07 15:49:31 +0000

    Skeleton application

---
+++ src/main/java/application/FantasyFootballApplication.java
@@ -0,0 +1,23 @@
+package main.java.application;
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class FantasyFootballApplication extends Application<FantasyFootballConfiguration>{
+
+	@Override
+	public void initialize(Bootstrap<FantasyFootballConfiguration> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run(FantasyFootballConfiguration arg0, Environment arg1)
+			throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/application/FantasyFootballConfiguration.java
@@ -0,0 +1,7 @@
+package main.java.application;
+
+import io.dropwizard.Configuration;
+
+public class FantasyFootballConfiguration extends Configuration{
+
+}
